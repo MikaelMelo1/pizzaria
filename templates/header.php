05c49218b8ef6bb32a08c4350352755aeaa7b452
Mikael Melo <mikaelsilva1940@hotmail.com>
2025-08-21 09:14:54 -0300

    feat: alerta de mensagem

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,17 @@
+<?php
+  session_start();
+  include("process/conn.php");
+  $msg = "";
+
+  if(isset($_SESSION['msg'])) {
+      $msg = $_SESSION['msg'];
+      $status = $_SESSION['status'];
+
+      $_SESSION['msg'] = "";
+      $_SESSION['status'] = "";
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,6 +50,9 @@
       </div>
     </nav>
   </header>
-  <div class="alert alert-sucess">
-    <p>Pedido feito com sucesso!</p>
-  </div>
+
+  <?php if($msg != ""): ?>
+    <div class="alert alert-<?= $status ?>">
+      <p><?= $msg ?></p>
+    </div>
+  <?php endif; ?>
