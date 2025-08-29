f763602fdd7b16be254afd4e6f6cabc7b0ec1b53
Mikael Melo <mikaelsilva1940@hotmail.com>
2025-08-29 11:42:48 -0300

    feat: atualizado conexão com o banco de dados

---
+++ process/conn.php
@@ -0,0 +1,15 @@
+<?php
+$user = "root";
+$pass = "admin";
+$db = "pizzaria";
+$host = "localhost";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?>
